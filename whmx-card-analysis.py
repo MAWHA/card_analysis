ea67e1fe051ff9c5d1408b6ab7ad857f1ddc3e02
liusuan233 <3145849381@qq.com>
2024-08-16 15:23:46 +0800

    修改部分代码

--- whmx-card-analysis.py
+++ whmx-card-analysis.py
@@ -1,52 +1,63 @@
-import requests
-import qrcode as qr
+from requests import post
+import qrcode
 import time
-import json
+from colorama import init,Fore,Back,Style
 
-url = "https://goda.srap.link/qrcode_login"
-check_url = "https://goda.srap.link/check_qrcode"
 
-headers = {
+class get_userinfo():
+    '''
+    获取用户用于登录的access_key和uid
+    '''
+    def __init__(self,):
+        '''
+        初始化
+        '''
+        self.url = "https://goda.srap.link/qrcode_login"
+        self.check_url = "https://goda.srap.link/check_qrcode"
+        self.card_info_url = "https://goda.srap.link/getDrawCardHistory"
+        self.headers = {
     'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
     'Accept': '*/*',
     'Host': 'goda.srap.link',
     'Connection': 'keep-alive',
-    'Cookie': 'sl-session=TXTgBgnZvmaetoutYaz9WA=='
-}
-
-def get_chouka_data():
-    response = requests.request("POST", url, headers=headers)
-    
-    if response.status_code == 200:
-        qrcode_data = response.json()['data']
-        # 将字典转换为字符串
-        qrcode = "biligame://portal/auth/login/qrcode?" + qrcode_data            
-        # 将二维码转换为图片
-        img = qr.make(qrcode)
-        img.save("qrcode.png")
+}    
         
-        # 打开二维码图片
+    def get_qrcode(self):
+        '''
+        获取二维码,返回ticket
+        '''
+        data = post(self.url, headers=self.headers)
+        if data.json()['status'] != True:
+            print("获取用户信息失败")
+        data = data.json()
+        qrcode_data = data.get('data')
+        user_info_url = "biligame://portal/auth/login/qrcode?" + qrcode_data
+        img = qrcode.make(user_info_url)
         img.show()
-        
-        payload1 = {
+        self.qrcode_data = qrcode_data
+        return qrcode_data
+
+    def check_qrcode(self,qrcode_data):
+        payload = {
             "ticket": str(qrcode_data)
         }
-        
-        # 等待扫码
         response_qrcode = None
         while not response_qrcode or  response_qrcode.json()['status']==False:
-            # 轮询检查扫码状态
-            response_qrcode = requests.post(check_url, headers=headers,json=payload1)
-            time.sleep(1)  # 每隔2秒检查一次
-        # 扫码成功
-        
-        access_key, uid = response_qrcode.json()['data']['access_key'], response_qrcode.json()['data']['uid']
+            response_qrcode = post(self.check_url, headers=self.headers,json=payload)
+            time.sleep(1)
+        self.account_info = response_qrcode.json()['data']
+    
+    def get_account_info(self):
+        access_key = self.account_info.get('access_key')
+        uid = self.account_info.get('uid')
+        return access_key, uid
+
+    def get_card_info(self,access_key,uid):
         pages = 1
-        data = []
-        while True:
-            # 轮询获取游戏数据
-            
-            payload2 = {
+        card_info = None
+        card_info_list = []
+        while card_info == None or card_info.json().get('message')!="发生错误":
+            payload = {
                     "code": str(access_key),
                     "uid": str(uid),
                     "page": pages,
@@ -55,66 +66,88 @@ def get_chouka_data():
                     "time"
                 ],
                 }
-            # 获取游戏数据
-            get_game_data = requests.post("https://goda.srap.link/getDrawCardHistory", headers=headers, json=payload2)
-            if "false" in get_game_data.text:
-                # 已经获取完所有游戏数据
-                print("已经获取完所有数据，等待整合...")
-                return data
-            time.sleep(1)  # 每隔1秒获取一次
-            # 获取游戏数据成功
-            data.append(str(get_game_data.text))
-            print("获取到{}页数据".format(pages))
-            pages += 1
-    else:
-        print("获取登录二维码失败")
-        return None, None
+            card_info = post(self.card_info_url, headers=self.headers,json=payload)
+            if card_info.json().get('data') != []: 
+                card_info_list.append(card_info.json().get('data'))
+                print(f"获取到第{pages}页数据")
+                pages += 1
+        return card_info_list
     
-    
-def data_analysis(data):
-    print("开始分析数据...")
-        # 分析数据
-    import json
-    list_data = []
-    # 假设你的嵌套JSON数据如下
-    for i in range(len(data)):
-        json_data = data[i]
 
-        game_data = json.loads(json_data)
-        for item in game_data['data']:
-            list_data.append([item["CardName"], item["Rare"], item["PoolName"]])
-        # poolnames = [item['PoolName'] for item in game_data['data']]
-        # cardnames = [item['CardName'] for item in game_data['data']]
-        # rares = [item['Rare'] for item in game_data['data']]
-        # print(poolnames, cardnames, rares)
-    print("分析完成")
-    # 打印分析结果
+class card_analysis():
+    '''
+    分析卡池信息
+    继承自get_userinfo类
+    '''
+    def analysis_card_info(self,card_info_list):
+        techu, youyi, xinsheng = 0,0,0
+        CardName_list = []
+        for card_info in card_info_list:
+            for i in card_info:
+                card_name = i.get('CardName')
+                rare = i.get('Rare')
+                if rare == 4:
+                    CardName_list.append(card_name)
+                    techu += 1
+                elif rare == 3:
+                    youyi += 1
+                elif rare == 2:
+                    xinsheng += 1
+        print("\033[32m抽卡总数:{}\033[0m".format(techu+youyi+xinsheng))
+        print("\033[32m出红概率:{:.2f}%\033[0m".format(float(techu/(techu+youyi+xinsheng))*100))
 
-    print("游戏数据如下：")
+        print(f"\033[31m特出器者:{techu}\033[0m",end=" ")
+        for i in CardName_list:
+            print(f"\033[31m{i}\033[0m",end=" ")
+        print(f"\033[33m\n优异器者:{youyi}\033[0m")
+        print(f"\033[34m新生器者:{xinsheng}\033[0m")
 
-    techu, youyi, xinsheng = 0, 0, 0
-    for i in range(len(list_data)):
-        if list_data[i][1] == 4:
-            techu += 1
-    print("特出器者:",techu)
-    for i in range(len(list_data)):
-        if list_data[i][1] == 3:
-            youyi += 1
-    print("优异器者:",youyi)
-    for i in range(len(list_data)):
-        if list_data[i][1] == 2:
-            xinsheng += 1
-    print("新生器者:",xinsheng)
-    #详细卡池数据：
-    while True:
-        accu = input("输入y查看详细卡池数据，输入s结束程序：")
-        if accu == "y":
-            print("详细卡池数据如下：")
-            for i in range(len(list_data)):
-                print(list_data[i])
-        elif accu == "s":
-            exit()
-        else:
-            print("输入错误，请重新输入")
+    def all_card_info(self,card_info_list):
+        '''
+        打印所有卡池信息
+        '''
+        all_card_info = []
+        for card_info in card_info_list:
+            for i in card_info:
+                pool_name = i.get('PoolName')
+                card_name = i.get('CardName')
+                rare = i.get('Rare')
+                all_card_info.append(f"卡池:{pool_name},器者:{card_name},稀有度:{rare}")
+                
+        return all_card_info
 if __name__ == '__main__':
-    data_analysis(get_chouka_data())
+    user_info = get_userinfo()
+
+    qrcode_data = user_info.get_qrcode()
+    user_info.check_qrcode(qrcode_data=qrcode_data)
+    access_key, uid = user_info.get_account_info()
+    card_info_list = user_info.get_card_info(access_key,uid)
+
+    card_analysis = card_analysis()
+    card_analysis.analysis_card_info(card_info_list)
+
+    user_input = None
+    while user_input ==None or user_input not in ['e']:
+        user_input = input("输入y查看所有卡池信息,输入s保存卡池信息到目录下card_info.txt文件,输入e退出:")
+        if user_input == 'y':
+            print(f"卡池信息如下:",end='\n')
+            for card_info in card_info_list:
+                for i in card_info:
+                    pool_name = i.get('PoolName')
+                    card_name = i.get('CardName')
+                    rare = i.get('Rare')
+                    if rare == 4:
+                        print(f"\033[31m卡池:{pool_name},器者:{card_name},稀有度:{rare}\033[0m")
+                    elif rare == 3:
+                        print(f"\033[33m卡池:{pool_name},器者:{card_name},稀有度:{rare}\033[0m")
+                    elif rare == 2:
+                        print(f"\033[34m卡池:{pool_name},器者:{card_name},稀有度:{rare}\033[0m")
+        elif user_input =='s':
+            with open('card_info.txt','w',encoding='utf-8') as f:
+                f.write("查询时间:{}".format(time.strftime('%Y-%m-%d %H:%M:%S'),end='\n'))
+                card_data = card_analysis.all_card_info(card_info_list)
+                for card_info in card_data:
+                    f.write(str(card_info))
+                    f.write('\n')
+            print("卡池信息保存成功")
+
